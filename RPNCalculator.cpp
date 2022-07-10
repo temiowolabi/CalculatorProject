bd857c6adcf1869e89c6ead5216ec8ef05f6f27a
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2022-07-10 20:03:27 +0100

    Added some implementations to header class functions

---
+++ RPNCalculator.cpp
@@ -0,0 +1,15 @@
+#include "RPNCalculator.h"
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+void menu() {
+	cout << "Project By: Temi Owolabi " << endl;
+	cout << "Welcome  to RPN Calculator: " << endl;
+	cout << "Available Operands are  " << endl;
+}
+
+int main() {
+
+}
--- RPNCalculator.h
+++ RPNCalculator.h
@@ -1,11 +1,17 @@
 #pragma once
-template<class T> class RPNCalculator
+template<class T> 
+class RPNCalculator
+
 {
 private:
 	/* NOTE:
 	* 1. You MUST use an instance of a Stack
 	* 2. The following functions are to be implemented as defined for RP
 	*/
+
+
+	T* data;
+	int topNum;
 public:
 	// pushes a new operand onto the stack
 	void push(T operand);
@@ -27,3 +33,80 @@ public:
 	// returns the topmost value and pops it off the top
 	T pop();
 };
+
+
+template <class T>
+void RPNCalculator<T>::add()
+{
+	if (values() >= 2)
+	{
+		push(pop() + pop());
+	}
+}
+
+
+template <class T>
+void RPNCalculator<T>::subtract()
+{
+	if (values() >= 2)
+	{
+		push(0 - pop() + pop());
+	}
+}
+
+
+template <class T>
+void RPNCalculator<T>::multiply()
+{
+	if (values() >= 2)
+	{
+		push(pop() * pop());
+	}
+}
+
+
+template <class T>
+void RPNCalculator<T>::divide()
+{
+	if (values() >= 2)
+	{
+		push(1 / pop() * pop());
+	}
+}
+
+
+template <class T>
+bool RPNCalculator<T>::isEmpty()
+{
+	return topNum == 0;
+}
+
+
+template <class T>
+void RPNCalculator<T>::clear()
+{
+	topNum = 0;
+}
+
+
+template <class T>
+T RPNCalculator<T>::value() {
+	if (topNum > 0)
+	{
+		return data[topNum - 1];
+	}
+
+	return 0;
+}
+
+
+template <class T>
+T RPNCalculator<T>::pop()
+{
+	T popped_element = st[top];
+
+	topNum--;
+
+	return popped_element;
+
+}
